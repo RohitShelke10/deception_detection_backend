77661a0ffc0f78d5fbda9cba7e4f2ff31205df86
Rohit <rohitshelke11@gmail.com>
2023-06-02 10:30:43 +0530

    added another origin

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ MODEL = keras.models.load_model('./LSTM_sigmoid/')
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['https://deception-detection.vercel.app'],
+    allow_origins=['https://deception-detection.vercel.app', 'http://127.0.0.1:5173'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
